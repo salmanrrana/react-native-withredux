440577e9ff4c28701ee8e266f2fb309651087bc2
Salman Rana <salman.r.rana@gmail.com>
2017-06-17 09:52:47 -0500

    able to recognize tap on listitem

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  //boiler plate set up of listView
+  //tells what source of data to use. knows how to figure out what items to show on the screen
+  //depending on where the user has scrolled on the screen
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    //have access to this.props.libraries because of the mapStatetoprops function below
+    //only part that is not boilerplate is the thiss.props.libraries
+    this.dataSource = ds.cloneWithRows(this.props.libraries)
+  }
+
+  //this will return a single library
+  renderRow(library) {
+    return (
+      <ListItem library={library} />
+    )
+  }
+
   render() {
     console.log('props: ', this.props);
-    return;
+    return(
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    )
   }
 }
 
 const mapStateToProps = state => {
   return { libraries: state.libraries };
-  console.log('state: ', state);
 };
 
 export default connect(mapStateToProps)(LibraryList);
---
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -0,0 +1,4 @@
+export default (state, action) => {
+  console.log('action in selectionreducer: ', action);
+  return null;
+};
